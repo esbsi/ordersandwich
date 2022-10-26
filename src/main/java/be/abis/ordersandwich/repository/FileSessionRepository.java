c8d63d170214e465dae18de107bf44c1f6d0c7d3
esbsi <esben.six@protonmail.com>
2022-10-26 16:40:24 +0200

    something funny

--- src/main/java/be/abis/ordersandwich/repository/FileSessionRepository.java
+++ src/main/java/be/abis/ordersandwich/repository/FileSessionRepository.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import java.io.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class FileSessionRepository implements SessionRepository {
@@ -43,9 +46,40 @@ public class FileSessionRepository implements SessionRepository {
     public Session findMostRecentSession(String sessionName) throws SessionNotFoundException {
         return sessions.stream()
                 .filter(session -> sessionName.equals(session.getName()))
-                .sorted(Collections.reverseOrder())
+                .sorted(Comparator.comparing(Session::getStartDate).reversed())
                 .findFirst().orElseThrow(SessionNotFoundException::new);
     }
+
+    @Override
+    public Session findSession(String sessionName, LocalDate startDate) throws SessionNotFoundException {
+        return sessions.stream()
+                .filter(session -> sessionName.equals(session.getName()) && startDate.isEqual(session.getStartDate()))
+                .findFirst().orElseThrow(SessionNotFoundException::new);
+    }
+
+    @Override
+    public List<Session> findSessionsByName(String sessionName) throws SessionNotFoundException {
+        List<Session> sessionsFiltered = sessions.stream()
+                .filter(session -> sessionName.equals(session.getName()))
+                .collect(Collectors.toList());
+         if (sessionsFiltered.size() == 0){
+             throw new SessionNotFoundException();
+         } else return sessionsFiltered;
+    }
+
+    @Override
+    public List<Session> findSessionsByDate(LocalDate localDate) throws SessionNotFoundException {
+        List<Session> sessionsFiltered = sessions.stream()
+                .filter(s -> s.getStartDate().minusDays(1).isBefore(localDate) && s.getEndDate().plusDays(1).isAfter(localDate))
+                .collect(Collectors.toList());
+        if (sessionsFiltered.size() == 0){
+            throw new SessionNotFoundException();
+        } else return sessionsFiltered;
+    }
+
+
+
+
 
     @Override
     public void addSession(String name, String startDate, String endDate) {
--- src/main/java/be/abis/ordersandwich/repository/SessionRepository.java
+++ src/main/java/be/abis/ordersandwich/repository/SessionRepository.java
@@ -3,12 +3,16 @@ package be.abis.ordersandwich.repository;
 import be.abis.ordersandwich.exception.SessionNotFoundException;
 import be.abis.ordersandwich.model.Session;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface SessionRepository {
 
-    List<Session> getSessions();
-    Session findMostRecentSession(String sessionName) throws SessionNotFoundException;
     void addSession(String name, String startDate, String endDate);
+    List<Session> getSessions();
+    Session findSession(String sessionName, LocalDate startDate) throws SessionNotFoundException;
+    Session findMostRecentSession(String sessionName) throws SessionNotFoundException;
+    List<Session> findSessionsByName(String sessionName) throws SessionNotFoundException;
+    List<Session> findSessionsByDate(LocalDate localDate) throws SessionNotFoundException;
 
 }
