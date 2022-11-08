787c1573811be10769c52fcb16756ba68d74f935
esbsi <esben.six@protonmail.com>
2022-11-08 12:21:56 +0100

    something funny

--- src/test/java/be/abis/ordersandwich/service/OrderTodayServiceTest2.java
+++ src/test/java/be/abis/ordersandwich/service/OrderTodayServiceTest2.java
@@ -3,9 +3,7 @@ package be.abis.ordersandwich.service;
 import be.abis.ordersandwich.exception.*;
 import be.abis.ordersandwich.model.OrderToday;
 import be.abis.ordersandwich.model.Person;
-import be.abis.ordersandwich.model.Session;
 import be.abis.ordersandwich.model.Shop;
-import be.abis.ordersandwich.repository.OrderHistory;
 import be.abis.ordersandwich.repository.OrderRepository;
 import be.abis.ordersandwich.repository.ShopRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,10 +17,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-public class StaffServiceTest {
+public class OrderTodayServiceTest2 {
 
-    @Autowired
-    StaffService staffService;
     @Autowired
     OrderTodayService orderTodayService;
     @Autowired
@@ -49,34 +45,13 @@ public class StaffServiceTest {
 
     @Test
     void sendOrder() throws SandwichTypeNotFoundException, TooLateException, TooManySandwichesException, NullInputException {
-        //orderTodayService.orderSandwich(1,true,true,"",person,orderToday);
-        orderToday = staffService.sendOrder(orderToday, history, shop2);
-        assertEquals(shop2,orderToday.getShop());
+        //orderTodayService.orderSandwich(1,true,true,"",person);
+        orderTodayService.sendOrder();
     }
 
     @Test
     void changeShop() throws SandwichTypeNotFoundException, TooLateException, TooManySandwichesException, NullInputException {
         assertEquals(shop,orderToday.getShop());
     }
-
-    @Test
-    void sendOrderWithNull1() throws SandwichTypeNotFoundException, TooLateException, TooManySandwichesException, NullInputException {
-        orderTodayService.orderSandwich(1,true,true,"",person);
-        orderToday = staffService.sendOrder(orderToday, history, shop2);
-        assertThrows(NullInputException.class,()->staffService.sendOrder(orderToday, history, null));
-    }
-    @Test
-    void sendOrderWithNull2() throws SandwichTypeNotFoundException, TooLateException, TooManySandwichesException, NullInputException {
-        orderTodayService.orderSandwich(1,true,true,"",person);
-        orderToday = staffService.sendOrder(orderToday, history, shop2);
-        assertThrows(NullInputException.class,()->staffService.sendOrder(orderToday, null, shop));
-    }
-    @Test
-    void sendOrderWithNull3() throws SandwichTypeNotFoundException, TooLateException, TooManySandwichesException, NullInputException {
-        orderTodayService.orderSandwich(1,true,true,"",person);
-        orderToday = staffService.sendOrder(orderToday, history, shop2);
-        assertThrows(NullInputException.class,()->staffService.sendOrder(null, history, shop));
-    }
-
 
 }
