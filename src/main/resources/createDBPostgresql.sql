66c7b0ad5a2b1e2ed41cf5be9236fabbade8bca5
esbsi <esben.six@protonmail.com>
2022-11-28 12:29:30 +0100

    something funny

--- src/main/resources/createDBPostgresql.sql
+++ src/main/resources/createDBPostgresql.sql
@@ -1,13 +1,26 @@
 DROP TABLE SHOPS CASCADE;
+DROP TABLE PERSONS CASCADE;
+DROP TABLE SESSIONs CASCADE;
+DROP TABLE sandwichorders CASCADE;
+DROP TABLE SANDWICHTYPES CASCADE;
+DROP TABLE orderHistory CASCADE;
+DROP TABLE sandwichorderstoday CASCADE;
+DROP TABLE sessionparticipants CASCADE;
 
-drop sequence  shop_shno_seq;
+drop sequence shop_seq;
+drop sequence person_seq;
+drop sequence session_seq;
+drop sequence sandwichtype_seq;
+drop sequence orderhistory_seq;
+drop sequence sandwichorder_seq;
+
 
 
 create sequence shop_seq start with 1;
 create sequence person_seq start with 1;
 create sequence session_seq start with 1;
 create sequence sandwichtype_seq start with 1;
-create sequence ordertoday_seq start with 1;
+create sequence orderhistory_seq start with 1;
 create sequence sandwichorder_seq start with 1;
 
 
@@ -15,7 +28,8 @@ CREATE TABLE SHOPS
 (ID      INT  primary key default nextval('shop_seq'),
 SHOP_NAME VARCHAR(45) NOT NULL
 );
-CREATE TABLE SESSIONs
+
+CREATE TABLE SESSIONS
 (ID      INT  primary key default nextval('session_seq'),
  session_name VARCHAR(45) NOT NULL,
  startdate date not null,
@@ -25,39 +39,68 @@ CREATE TABLE SESSIONs
 CREATE TABLE PERSONS
 (ID      INT  primary key default nextval('person_seq'),
  firstname VARCHAR(45) NOT NULL,
- startdate date not null,
- enddate date not null
+ lastname varchar(1000) NOT NULL
 );
 
-CREATE TABLE SANWICHTYPES
+CREATE TABLE SANDWICHTYPES
 (ID      INT  primary key default nextval('sandwichtype_seq'),
- sandwichname VARCHAR(45) NOT NULL,
+ sandwich_name VARCHAR(45) NOT NULL,
  shop_id INT ,
  CONSTRAINT FK_shop FOREIGN KEY (shop_id) REFERENCES shops,
  price double precision,
- typecategory varchar(45),
+ sandwich_category varchar(45),
  vegetarian varchar(1),
  constraint boolean_veggie check ( vegetarian in ('T','F') ),
- discription varchar(1000)
-
+ description varchar(1000)
 );
 
-
 create table orderHistory
-(ID      INT  primary key default nextval('ordertoday_seq'),
+(ID      INT  primary key default nextval('orderhistory_seq'),
 totalprice double precision,
  shop_id INT ,
  CONSTRAINT FK_shop2 FOREIGN KEY (shop_id) REFERENCES shops,
  senddate date,
  closingtime time
-
-
 );
 
-INSERT INTO SHOPS VALUES (nextval('shop_shno_seq'), 'Pinkys');
-INSERT INTO SHOPS VALUES (nextval('shop_shno_seq'), 'Vleugels');
+create table sandwichorders
+(
+    id              int primary key default nextval('sandwichorder_seq'),
+    sandwichtype_id int,
+    constraint FK_sandwichtype foreign key (sandwichtype_id) references SANDWICHTYPES,
+    rauwkost        varchar(1),
+    constraint boolean_rauwkost check ( rauwkost in ('T', 'F') ),
+    grilledvegs     varchar(1),
+    constraint boolean_grilledvegs check ( grilledvegs in ('T', 'F') ),
+    white           varchar(1),
+    constraint boolean_white check ( white in ('T', 'F') ),
+    comment         varchar(1000),
+    person_id       int,
+    constraint FK_person foreign key (person_id) references PERSONS
+);
+     
+create table sandwichorderstoday
+(
+    sandwichorder_id int,
+    constraint FK_sandwichorder foreign key (sandwichorder_id) references sandwichorders,
+    orderhistory_id int,
+    constraint FK_orderHistory foreign key (orderhistory_id) references orderHistory
+);
+
+create table sessionparticipants
+(
+    session_id int,
+    constraint FK_session foreign key (session_id) references SESSIONs,
+    person_id int,
+    constraint FK_person foreign key (person_id) references persons
+);
+     
+
+ INSERT INTO SHOPS VALUES (nextval('shop_seq'), 'Pinkys');
+INSERT INTO SHOPS VALUES (nextval('shop_seq'), 'Vleugels');
 
 
+insert into PERSONS (firstname, lastname) values ('Joske', 'Demeuleneire') ;
 
 
 
